1b9e93d6a6adc7cc8e592b502783d37c8384cc56
JovialHall <33130634+JovialHall@users.noreply.github.com>
2017-10-26 19:37:02 -0400

    Lab 5 Step
    My own level and color edits

--- code/game.js
+++ code/game.js
@@ -26,6 +26,9 @@ function Level(plan) {
       else if (ch == "!")
         fieldType = "lava";
 
+      else if (ch == "y")
+        fieldType = "floater";
+
       // "Push" the fieldType, which is a string, onto the gridLine array (at the end).
       gridLine.push(fieldType);
     }
